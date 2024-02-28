7933d489195b598811643cd55a82788c7d88ae1e
Nathnael Bekele <nwtbekele@gmail.com>
2024-02-27 22:02:10 -0600

    updates

--- src/base_gui.py
+++ src/base_gui.py
@@ -1,11 +1,11 @@
-from tkinter import filedialog, Tk, Menu, ttk, Toplevel
+from tkinter import filedialog, Tk, Menu, ttk, Toplevel, Listbox, Text, scrolledtext
+from tkinter.constants import *
 from backup import Backup
 from werkzeug import Request, Response, run_simple
 import threading, time, os, webbrowser, ctypes
 from box_dialog_gui import BoxNav
 
 class BaseGui:
-
     '''
     Class for the Window of the App. 
     '''
@@ -47,12 +47,12 @@ class BaseGui:
         self.base = Tk('BSci Backup')
         self.base.protocol("WM_DELETE_WINDOW", self._on_closing)
         self.base.title('BSci Backup Application')
-        self.base.geometry("600x400")
-
-        self.v_scroll = ttk.Scrollbar(self.base, orient='vertical')
-        self.v_scroll.pack(side = 'right', fill = 'y')
+        self.base.geometry("600x510")
+        self.base.minsize(600,510)
+    
         self.frm = ttk.Frame(self.base, padding=10)
         self.frm.pack()
+
         self.auth_button = ttk.Button(self.frm,text="Authenticate", command=self.open_link, padding=5)
         self.auth_button.pack()
 
@@ -66,11 +66,11 @@ class BaseGui:
         Method that will run the oauth_server thread. Receives the csrf and code tokens, uses the backup object instance
         to authorize the user to use the BOX app.
         '''
-        print(threading.current_thread())
+        # print(threading.current_thread())
 
         @Request.application
         def app(request: Request) -> Response:
-            print("STARTED", self.backup.authorized, threading.current_thread())
+            # print("STARTED", self.backup.authorized, threading.current_thread())
             
             if self.backup.authorized:
                 return Response("Already Authorized", 200)
@@ -87,12 +87,12 @@ class BaseGui:
                         return Response("Authorization Denied", 200)
                 else:
                     self.authorization_status = "Completed"
-                    print("INVALID TOKENS", code, csrf, 2)
+                    # print("INVALID TOKENS", code, csrf, 2)
 
                     return Response("Authorization Denied", 200)
             except:
                 self.authorization_status = "Completed"
-                print("INVALID TOKENS", code, csrf, 3)
+                # print("INVALID TOKENS", code, csrf, 3)
 
                 return Response("Authorization Denied", 200)
 
@@ -129,7 +129,6 @@ class BaseGui:
                 ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(self.oauth_server.ident), ctypes.py_object(SystemExit))
                 self.backup_page()
             else:
-                print("Failed Authorization")
                 self.retry.pack()
                 self.auth_button["state"] = 'normal'
         else:
@@ -143,7 +142,6 @@ class BaseGui:
         - Backup button to trigger the backup. This diables all other buttons.
         '''
         frm = ttk.Frame(self.base, padding=10)
-        # frm.bind("<Configure>", on_configure)
         frm.pack()
         
         menu_bar = Menu(frm)
@@ -155,91 +153,54 @@ class BaseGui:
         menu_bar.add_command(label="Set Box Backup Folder", command = lambda sbf = selected_backup_folder: self.set_box_folder(sbf))
 
         ttk.Label(frm, text='Selected Folders to back up:', padding=10).pack()
-        folder_frm = ttk.Frame(frm)
+        folder_frm = ttk.Frame(frm, padding=5)
         folder_frm.pack()
-        folder_frm.config(height=0, padding=5)
-        menu_bar.add_command(label="Select Folders to Backup", command=lambda: self.select_folder(folder_frm))
+
+        self.folder_list = Listbox(folder_frm, height=7, selectmode='multiple')
+        self.folder_list.pack(side='left', fill='both', expand=True, ipadx=2, ipady=2)
+        sb_folder= ttk.Scrollbar(folder_frm)
+        sb_folder.pack(side='right', fill='y')
+        self.folder_list.config(yscrollcommand=sb_folder.set)
+        sb_folder.config(command=self.folder_list.yview)
+
+        menu_bar.add_command(label="Select Folders to Backup", command= self.select_folder)
 
         ttk.Label(frm, text='Selected Files to back up:', padding=10).pack()
-        file_frm = ttk.Frame(frm)
+        file_frm = ttk.Frame(frm, padding=5)
         file_frm.pack()
-        file_frm.config(height=0, padding=5)
-        menu_bar.add_command(label="Select Files to Backup", command=lambda: self.select_files(file_frm))
-        
-        backup_button = ttk.Button(frm, text="Backup", padding=5)
-        backup_button['command'] = lambda : self.call_backup(file_frm, folder_frm, backup_button) 
-
-        backup_button.pack()
     
-    def set_box_folder(self, selected_backup_folder):
-        '''
-        Creates an instance of the BoxNav object to open a dialog box for navigating box and selecting a new backup directory.
-        '''
-        if self.state != "Backing Up":
-            self.box_nav = BoxNav(self.backup, self.base, selected_backup_folder)
-            self.box_nav.display_folders()
+        self.file_list = Listbox(file_frm, height=9, selectmode='multiple')
+        self.file_list.pack(side='left', fill='both', expand=True, ipadx=2, ipady=2)
+        sb_file= ttk.Scrollbar(file_frm)
+        sb_file.pack(side='right', fill='y')
+        self.file_list.config(yscrollcommand=sb_file.set)
+        sb_file.config(command=self.file_list.yview)
 
-    def call_backup(self,folder_frm: ttk.Frame, file_frm: ttk.Frame, backup_button: ttk.Button):
-        '''
-        If there are any files/folders selected for backup, starts the backup_thread and updates the state of the app.
-        '''
-        if self.selected_files or self.selected_folders:
-            backup_button['state'] = 'disabled'
-            self.state = 'Backing Up'
-            backup_thread = threading.Thread(target=self.handle_backup, args=(file_frm, folder_frm, backup_button))
-            backup_thread.start()
+        menu_bar.add_command(label="Select Files to Backup", command= self.select_files)
         
+        frm_buttons = ttk.Frame(frm)
+        frm_buttons.pack()
+        self.remove_button = ttk.Button(frm_buttons, text="Delete", padding=5, command= self.remove_selected)
+        self.remove_button.pack(side='right', pady=5)
 
-    def handle_backup(self, folder_frm: ttk.Frame, file_frm: ttk.Frame, backup_button: ttk.Button):
-        '''
-        Uses the backup instance of the Backup class to back up each selected folder and file. Keeps track of the logs from 
-        backing up in the logs list and displays them in a dialog window when completed.
-        '''
-        logs = []
-        
-        temp = self.selected_folders[:]
-        for folder in temp:
-            response = self.backup.backup_folders(folder)
-            self.selected_folders.remove(folder)
-            logs.append(os.path.split(folder)[-1] + ": " + response)
-        
-        if len(logs) > 0:
-            logs.append("________________")
-        
-        temp = self.selected_files[:]
-        for file in temp:
-            response = self.backup.backup_files(file)
-            self.selected_files.remove(file)
-            logs.append(os.path.split(file)[-1] + ": " + response)
-
-        self.display_logs(logs)
-        backup_button['state'] = 'normal'
-
-        for widget in folder_frm.winfo_children():
-            widget.destroy()
-        for widget in file_frm.winfo_children():
-            widget.destroy()
-        
-        file_frm.config(height=0, padding=5)
-        folder_frm.config(height=0, padding=5)
-        file_frm.update_idletasks()
-        folder_frm.update_idletasks()
-        self.state = "Processing"
+        self.backup_button = ttk.Button(frm_buttons, text="Backup", padding=5, command=self.call_backup)
+        self.backup_button.pack(side='left', pady=5)
     
-    def display_logs(self, logs):
+    def remove_selected(self):
         '''
-        Displays the logs from backing up
+        Method to remove selected files or folders from the Listbox and selected folder/file lists.
         '''
-        if logs:
-            log_dialog = Toplevel(self.base)
-            log_dialog.geometry('400x300')
-            log_dialog.title('Log')
-            log_frm = ttk.Frame(log_dialog)
-            log_frm.pack()
-            for log in logs:
-                ttk.Label(log_frm, text=log, padding=5).pack()
+        if self.folder_list.curselection() != "" and self.selected_folders:
+            for ind in self.folder_list.curselection()[::-1]:
+                del self.selected_folders[ind]
+                self.folder_list.delete(ind)
+    
+        if self.file_list.curselection() != "" and self.selected_files:
+            for ind in self.file_list.curselection()[::-1]:
+                del self.selected_files[ind]
+                self.file_list.delete(ind)
 
-    def select_folder(self, folder_frm: ttk.Frame):
+    def select_folder(self):
         '''
         Opens the windows dialog for selecting a folder and adds any selected folder to the selected_folders list.
         Displays each folder's name along with a button to remove it from the list of folders to be backedup.
@@ -249,22 +210,22 @@ class BaseGui:
             
             def remove_folder(sub_frm: ttk.Frame, backup_folder: str):
                 if self.state != 'Backing Up':
-                    print(backup_folder, self.selected_folders)
+                    # print(backup_folder, self.selected_folders)
                     sub_frm.destroy()
                     self.selected_folders.remove(backup_folder)
 
             if backup_folder:
                 if backup_folder not in self.selected_folders:
                     self.selected_folders.append(backup_folder)
-                    sub_frm = ttk.Frame(folder_frm)
-                    sub_frm.pack()
-
+                    sub_frm = ttk.Frame(self.folder_list)
                     ttk.Label(sub_frm, text=os.path.split(backup_folder)[1], padding = 5).pack(side="left")
                     ttk.Button(sub_frm, text="x", 
                             command = lambda cur_folder = backup_folder, cur_frm = sub_frm: remove_folder(cur_frm, cur_folder), 
                             width=2, padding = 2).pack(side="right")
+                    
+                    self.folder_list.insert(len(self.selected_folders) - 1, os.path.split(backup_folder)[1])
 
-    def select_files(self, file_frm: ttk.Frame):
+    def select_files(self):
         '''
         Opens the windows dialog for selecting multiple files and adds any selected files to the selected_files list.
         Displays each files's name along with a button to remove it from the list of files to be backedup.
@@ -274,20 +235,93 @@ class BaseGui:
             
             def remove_file(sub_frm: ttk.Frame, file: str):
                 if self.state != 'Backing Up':
-                    print(file)
+                    # print(file)
                     sub_frm.destroy()
                     self.selected_files.remove(file)
 
             for file in backup_files:
                 if file not in self.selected_files:
                     self.selected_files.append(file)
-                    sub_frm = ttk.Frame(file_frm)
-                    sub_frm.pack()
+                    sub_frm = ttk.Frame(self.file_list)
+                    
 
                     ttk.Label(sub_frm, text=os.path.split(file)[1], padding=5).pack(side="left")    
                     ttk.Button(sub_frm, text="x", 
                             command= lambda cur_file= file, cur_frm = sub_frm: remove_file(cur_frm, cur_file), 
                             width=2, padding = 2).pack(side="right")
+                    self.file_list.insert(len(self.selected_files) - 1, os.path.split(file)[1])
+    
+    def set_box_folder(self, selected_backup_folder):
+        '''
+        Creates an instance of the BoxNav object to open a dialog box for navigating box and selecting a new backup directory.
+        '''
+        if self.state != "Backing Up":
+            self.box_nav = BoxNav(self.backup, self.base, selected_backup_folder)
+            self.box_nav.display_folders()
+
+    def call_backup(self):
+        '''
+        If there are any files/folders selected for backup, starts the backup_thread and updates the state of the app.
+        '''
+        if self.selected_files or self.selected_folders:
+            self.backup_button['state'] = 'disabled'
+            self.remove_button['state'] = 'disabled'
+
+            self.state = 'Backing Up'
+            backup_thread = threading.Thread(target=self.handle_backup)
+            backup_thread.start()
+        
+
+    def handle_backup(self):
+        '''
+        Uses the backup instance of the Backup class to back up each selected folder and file. 
+        Updates the logs and prints the status of the latest backedup folder/file.
+        '''
+        log_dialog = Toplevel(self.base)
+        log_dialog.geometry('500x400')
+        log_dialog.title('Log')
+
+        log_text = Text(log_dialog, height=250, wrap='none', padx=5, pady=5, font=("Arial", 11), state='disabled')
+        log_text.pack(ipadx=5, ipady=5)
+
+        temp_folders = self.selected_folders[:]
+        if len(temp_folders) > 0:
+            log_text.configure(state='normal')
+            log_text.insert(END, "Folders:\n" )
+            log_text.configure(state='disabled')
+
+        for folder in temp_folders:
+            response = self.backup.backup_folders(folder)
+            self.selected_folders.remove(folder)
+            log_text.configure(state='normal')
+            log_text.insert(END, "\t- " + os.path.split(folder)[-1] + ": " + response + '\n')
+            log_text.configure(state='disabled')
+
+        
+        temp_files = self.selected_files[:]
+        if len(temp_files) > 0:
+            log_text.configure(state='normal')
+            log_text.insert(END, "Files:\n" )
+            log_text.configure(state='disabled')
+        
+        for file in temp_files:
+            response = self.backup.backup_files(file)
+            self.selected_files.remove(file)
+            log_text.configure(state='normal')
+            log_text.insert(END, "\t- " + os.path.split(file)[-1] + ": " + response + '\n')
+            log_text.configure(state='disabled')
+
+        log_text.configure(state='normal')
+        log_text.insert(END, "\n\t\tCOMPLETED!\n")
+        log_text.configure(state='disabled')
+
+        self.backup_button['state'] = 'normal'
+        self.remove_button['state'] = 'normal'
+
+        self.folder_list.delete(0, 'end')
+        self.file_list.delete(0, 'end')
+        
+        self.state = "Processing"
 
     def _on_closing(self):
         '''
@@ -295,7 +329,10 @@ class BaseGui:
         '''
         for active_thread in threading.enumerate():
             if active_thread.name != "MainThread":
-                ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(active_thread), ctypes.py_object(SystemExit))
+                try:
+                    ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(active_thread), ctypes.py_object(SystemExit))
+                except:
+                    pass
             
         self.base.destroy()
     
